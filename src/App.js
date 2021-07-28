d6c56eb6a762875a6442fd0c4fd5ec3a587bccd6
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2021-07-28 16:43:41 -0400

    create Tasks and Task components, add deleteTask functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react'
+
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "Doctors appointment",
+        day: "Feb 5th at 2:30 pm",
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Soccer practice",
+        day: "September 8th at 5:00 PM",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Clean pool",
+        day: "November 11th at 11AM",
+        reminder: true
+    }
+])
+
+//Delete Task
+
+const deleteTask = function(id) {  
+   setTasks(tasks.filter((task) => task.id !== id))
+}
+
+//Toggle Reminder
+
+
 
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete = {deleteTask}/> : 'No Tasks to Show'}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 
 const Header = ({ title } ) => {
-    return (
+  const onClick = (e) => {
+    console.log(e);
+  }  
+  return (
       <header className='header'>
         <h1>{title}</h1>
-        <button className= 'btn'>Add</button>
+        <Button color='green' text='Add' onClick= {onClick}/>
       </header>
     )
 }
-
+ 
 Header.defaultProps = {
     title: 'Task Tracker'
 }
